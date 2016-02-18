7af8b12782a6a386fedcad32f02bef9e4fdc1a10
randint <xkcdrom@gmail.com>
2016-02-17 16:25:45 -0800

    Primes fun

--- python/prime-alg.py
+++ python/prime-alg.py
@@ -1,24 +1,36 @@
-import math
+from math import *
 
 plist = []
 
-def is_prime(n):
-    if n < 100:
-        return n in plist
-    for f in plist:
-        if n % f == 0:
+def is_prime(n, use_plist=True):
+    if use_plist:
+        if n < 100:
+            return n in plist
+        for f in plist:
+            if n % f == 0:
+                return False
+        # check this range
+        for f in range(102, int(sqrt(n)) + 1, 6):
+            if n % (f - 1) == 0 or n % (f + 1) == 0:
+                return False
+    else:
+        if n < 2:
             return False
-    for f in range(102, round(int(math.sqrt(n))), 6):
-        if n % (f - 1) == 0 or n % (f + 1) == 0:
-            return False    
+        if n == 2 or n == 3:
+            return True
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        for f in range(5, int(sqrt(n)) + 1, 6):
+            if n % (f - 1) == 0 or n % (f + 1) == 0:
+                return False
     return True
 
 
 # [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
 def generate_primes(limit):
-    primes = []
-    for i in range(2, limit + 1):
-        if is_prime(i):
+    primes = [2]
+    for i in range(3, limit + 1):
+        if is_prime(i, False):
             primes.append(i)
     return primes
 
